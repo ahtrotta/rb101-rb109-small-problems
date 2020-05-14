3cf37c64b04248d2d4a820f42766d87ebd514f89
Adam Trotta <ahtrotta@gmail.com>
2020-05-14 17:28:57 -0400

    started easy4 exercises

---
+++ easy4/short_long_short.rb
@@ -0,0 +1,11 @@
+def str_aba(a, b)
+  a + b + a
+end
+
+def short_long_short(str1, str2)
+  str1.length > str2.length ? str_aba(str2, str1) : str_aba(str1, str2)
+end
+
+puts short_long_short('abc', 'defgh') == 'abcdefghabc'
+puts short_long_short('abcde', 'fgh') == 'fghabcdefgh'
+puts short_long_short('', 'xyz') == 'xyz'
