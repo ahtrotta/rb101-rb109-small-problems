1ccd78a59a7bee4ff0ef325594282ea1b1151fb2
ahtrotta <ahtrotta@gmail.com>
2020-06-03 20:24:28 -0400

    almost done with fibonacci sequence

---
+++ medium1/fibonacci_numbers_procedural.rb
@@ -0,0 +1,10 @@
+def fibonacci(n)
+  arr = [1, 1]
+  (n - 2).times { arr[0], arr[1] = arr[1], arr[0] + arr[1] }
+  arr[1]
+end
+
+p fibonacci(1) == 1
+p fibonacci(2) == 1
+p fibonacci(20) == 6765
+p fibonacci(100) == 354224848179261915075
