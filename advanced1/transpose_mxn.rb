efaa76e3bb7a41e48d17e1829462232af9af8ca6
ahtrotta <ahtrotta@gmail.com>
2020-06-06 18:20:52 -0400

    completed transpose mxn

---
+++ advanced1/transpose_mxn.rb
@@ -0,0 +1,23 @@
+def transpose(mat)
+  transpose = mat[0].map { |_| [] }
+  mat.each do |row|
+    row.each_with_index { |el, i| transpose[i] << el }
+  end
+  transpose
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+   [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
