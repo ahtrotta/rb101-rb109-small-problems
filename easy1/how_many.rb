615b896e572c1d03e892dea367933bfbf785e3fa
ahtrotta <ahtrotta@gmail.com>
2020-06-15 15:46:58 -0400

    implemented a more concise solution

--- easy1/how_many.rb
+++ easy1/how_many.rb
@@ -3,17 +3,23 @@ vehicles = [
   'motorcycle', 'motorcycle', 'car', 'truck'
 ]
 
-def count_occurences(array)
-  hash = Hash.new
+# def count_occurences(array)
+#   hash = Hash.new
+# 
+#   array.each do |elem| 
+#     if hash[elem]
+#       hash[elem] += 1
+#     else
+#       hash[elem] = 1
+#     end
+#   end
+#   hash.each { |key, value| puts "#{key} => #{value}" }
+# end
 
-  array.each do |elem| 
-    if hash[elem]
-      hash[elem] += 1
-    else
-      hash[elem] = 1
-    end
-  end
+def count_occurences(arr)
+  hash = arr.each_with_object(Hash.new(0)) { |auto, hsh| hsh[auto] += 1 }
   hash.each { |key, value| puts "#{key} => #{value}" }
+  nil
 end
 
 count_occurences(vehicles)
