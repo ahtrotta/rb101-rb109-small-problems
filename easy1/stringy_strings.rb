f3f614ee62516acdcb17b6e39384b55ab4961ffe
ahtrotta <ahtrotta@gmail.com>
2020-06-15 16:38:55 -0400

    implemented a recursive solution for stringy strings

--- easy1/stringy_strings.rb
+++ easy1/stringy_strings.rb
@@ -1,10 +1,18 @@
+#def stringy(int, opt=1)
+#  string = ''
+#  int.times do |index|
+#    string << '1' if index % 2 == (1 - opt)
+#    string << '0' if index % 2 == (0 + opt)
+#  end
+#  string
+#end
+
 def stringy(int, opt=1)
-  string = ''
-  int.times do |index|
-    string << '1' if index % 2 == (1 - opt)
-    string << '0' if index % 2 == (0 + opt)
-  end
-  string
+  base = (opt == 1 ? '1' : '0')
+  chunk = (opt == 1 ? '10' : '01')
+  return '' if int == 0
+  return base if int == 1
+  chunk + stringy(int - 2, opt)
 end
 
 puts stringy(6) == '101010'
