257c362d4b7c4cef062535af3272518b914ac306
Adam Trotta <ahtrotta@gmail.com>
2020-06-15 20:52:15 -0400

    added alternate solution

--- easy3/counting_the_number_of_characters.rb
+++ easy3/counting_the_number_of_characters.rb
@@ -1,6 +1,7 @@
 print 'Please write a word or multiple words: '
 input = gets.chomp
 
+# count = input.split.map(&:size).inject(:+)
 count = input.delete(' ').size
 
 puts "There are #{count} characters in \"#{input}\"."
