e71f0ace0220f402461e1a10e23df12be19b5a25
ahtrotta <ahtrotta@gmail.com>
2020-06-16 17:20:56 -0400

    streamlined solution to short long short

--- easy4/short_long_short.rb
+++ easy4/short_long_short.rb
@@ -2,8 +2,12 @@ def str_aba(a, b)
   a + b + a
 end
 
-def short_long_short(str1, str2)
-  str1.length > str2.length ? str_aba(str2, str1) : str_aba(str1, str2)
+#def short_long_short(str1, str2)
+#  str1.length > str2.length ? str_aba(str2, str1) : str_aba(str1, str2)
+#end
+
+def short_long_short(a, b)
+  a.length > b.length ? b + a + b : a + b + a
 end
 
 puts short_long_short('abc', 'defgh') == 'abcdefghabc'
