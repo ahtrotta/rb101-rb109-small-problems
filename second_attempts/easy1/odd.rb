f4271277e57852fac48c4278a0defa55ce348ae7
ahtrotta <ahtrotta@gmail.com>
2020-06-25 11:20:09 -0400

    completed odd

---
+++ second_attempts/easy1/odd.rb
@@ -0,0 +1,33 @@
+=begin
+
+PROBLEM:
+
+INPUT(S):
+
+OUTPUTS(S):
+
+ALGORITHM:
+
+=end
+
+#def is_odd?(int)
+#  int % 2 == 1
+#end
+
+#def is_odd?(int)
+#  _, remainder = int.divmod(2)
+#  remainder == 1
+#end
+
+def is_odd?(int)
+  (int / 2) == ((int - 1) / 2)
+end
+
+# test cases
+
+puts is_odd?(2) == false    # => false
+puts is_odd?(5) == true     # => true
+puts is_odd?(-17) == true   # => true
+puts is_odd?(-8) == false   # => false
+puts is_odd?(0) == false    # => false
+puts is_odd?(7) == true     # => true
