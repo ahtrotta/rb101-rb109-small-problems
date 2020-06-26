771c9e026dbf16dd1c3569ec149bc9be7f2b6323
ahtrotta <ahtrotta@gmail.com>
2020-06-26 13:44:31 -0400

    completed arithmetic_integer.rb

---
+++ second_attempts/easy3/arithmetic_integer.rb
@@ -0,0 +1,29 @@
+=begin
+
+PROBLEM:
+
+INPUT(S):
+
+OUTPUTS(S):
+
+ALGORITHM:
+
+=end
+
+def prompt(msg)
+  puts "==> #{msg}"
+end
+
+prompt '==> Enter the first number'
+first = gets.chomp.to_i
+
+prompt '==> Enter the second number:'
+second = gets.chomp.to_i
+
+prompt "#{first} + #{second} = #{first + second}"
+prompt "#{first} - #{second} = #{first - second}"
+prompt "#{first} * #{second} = #{first * second}"
+prompt "#{first} / #{second} = #{first / second}"
+prompt "#{first} % #{second} = #{first % second}"
+prompt "#{first} ** #{second} = #{first ** second}"
+
