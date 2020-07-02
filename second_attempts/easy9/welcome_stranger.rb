21111e8c6958c201295e429144aa643cb608cf5d
ahtrotta <ahtrotta@gmail.com>
2020-07-02 13:34:32 -0400

    completed welcome stranger

---
+++ second_attempts/easy9/welcome_stranger.rb
@@ -0,0 +1,20 @@
+=begin
+
+PROBLEM:
+
+INPUT(S):
+
+OUTPUTS(S):
+
+ALGORITHM:
+
+=end
+
+def greetings(arr, hsh)
+  puts "=> Hello, #{arr.join(' ')}! " \
+       "Nice to have a #{hsh[:title]} #{hsh[:occupation]} around."
+end
+
+# test cases
+
+greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
