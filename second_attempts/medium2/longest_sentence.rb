eff7211f5de52697c2c799422146e9a461c01b82
ahtrotta <ahtrotta@gmail.com>
2020-07-04 15:50:53 -0400

    completed longest sentence

--- second_attempts/medium2/longest_sentence.rb
+++ second_attempts/medium2/longest_sentence.rb
@@ -7,12 +7,21 @@ INPUT(S):
 OUTPUTS(S):
 
 ALGORITHM:
+- split string into sentences and store in array
+- sort array by sentence length
+- return length of last sentence
 
 =end
 
-def
-end
+#f = File.open('frankenstein.txt')
+f = File.open('four_score.txt')
 
-# test cases
+str = ''
+f.each_line { |line| str << line.chomp + ' ' }
 
-puts 
+arr = str.split('.').map { |sent| sent.split('!') }.flatten.map { |sent| sent.split('?') }.flatten
+
+
+longest = arr.sort_by { |sentence| sentence.split.size }.last.strip
+puts ">> The longest sentence is #{longest.split.size} words:"
+puts "\n#{longest}"
